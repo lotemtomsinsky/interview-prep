846f19c4805d6b7844214a478a8df416845e3bf7
lotemtomsinsky <lotem.tomsinsky@gmail.com>
2025-03-11 17:27:28 -0400

    was missing sliding window approach, only a hashset isn't enough to cover possible edge cases. passing all tests

--- LongestSubstrNoRepeatChar.java
+++ LongestSubstrNoRepeatChar.java
@@ -7,29 +7,31 @@ public class LongestSubstrNoRepeatChar {
     public static int lengthOfLongestSubstring(String s) {
         HashSet<Character> strContent = new HashSet<>();
         int maxLength = 0;
+        int left = 0;
 
-        for (char c: s.toCharArray())
+        for (int right = 0; right < s.length(); right++)
         {
-            if (!strContent.contains(c))
+            char c = s.charAt(right);
+            while (strContent.contains(c))
             {
-                strContent.add(c);
+                strContent.remove(s.charAt(left));
+                left++;
             }
-            else
-            {
-                maxLength = Math.max(maxLength, strContent.size());
-                strContent.clear();
-                strContent.add(c); // otherwise we skip the letter we're currently looking at as part of a potential substring
-            }
-        }
 
+            strContent.add(c);
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+        
         return maxLength;
     }
 
     public static void main(String[] args)
     {
         //String s = "zxyzxyz"; // expect 3
-        //String s = "xxxx"; // expect 0
+        //String s = "xxxx"; // expect 1
         String s="pwwkew"; // expect 3
+        //String s = " "; // expect 1
+        //String s = "dvdf"; // expect 3
 
         System.out.println(lengthOfLongestSubstring(s));
     }
